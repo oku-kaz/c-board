224376b23cb605106e92fe6a4c7f28142791ceb4
oku-kaz <okusaku.sd@gmail.com>
2015-07-01 13:37:35 +0900

    カスタムルートの実験中

--- c-board/App_Start/RouteConfig.cs
+++ c-board/App_Start/RouteConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.Routing;
 using Microsoft.AspNet.FriendlyUrls;
+using c_board.Models.Routing;
 
 namespace c_board
 {
@@ -10,9 +11,8 @@ namespace c_board
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            var settings = new FriendlyUrlSettings();
-            settings.AutoRedirectMode = RedirectMode.Permanent;
-            routes.EnableFriendlyUrls(settings);
+            routes.Add(new Route("foo/{shop_code}/{*action}", new fooRouteHandler("~/foo/{action}.aspx")));
+            routes.EnableFriendlyUrls();
         }
     }
 }
---
+++ c-board/foo/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="c_board.foo.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    ショップコード<%= shopCode %>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
